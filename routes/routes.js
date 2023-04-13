d07ebd93e9ffc7a1c580b0b911a6795a8986e326
Sam Cowley <sfcowley@gmail.com>
2023-04-13 10:20:56 +0100

    quantababy airtable deets added to env. New routes for tables added and refactored into a setupRoutes function. added a public folder and index

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,30 @@
 import express from 'express';
-import createCrud from '../middleware/airtableCrud.js'
+import createCrud from '../middleware/airtableCrud.js';
+import '../env.js';
 
 const router = express.Router();
 
-// Change 'Meals' to your table name
-const table = createCrud('all Values');
+// tables
+const feed = process.env.TABLE_FEED;
+const feedStart = process.env.TABLE_FEED_START;
+const feedStop = process.env.TABLE_FEED_STOP;
+const sleep = process.env.TABLE_SLEEP;
 
-router.get('/', table.find(), (req, res) => res.jsonp(req.result));
-router.get('/new', (req, res) => res.render('new'));
-router.post('/new', table.create(), (req, res) => res.jsonp(req.result));
-router.get('/:id', table.findOne(), (req, res) => res.jsonp(req.result));
-router.put('/:id', table.update(), (req, res) => res.jsonp(req.result));
+// create routes for each table
+function setupRoutes(tableName, routePath) {
+  const crudInstance = createCrud(tableName);
+  router.get(`/${routePath}/`, crudInstance.find(), (req, res) => res.jsonp(req.result));
+  router.get(`/${routePath}/new`, (req, res) => res.render('new'));
+  router.post(`/${routePath}/new`, crudInstance.create(), (req, res) => res.jsonp(req.result));
+  router.get(`/${routePath}/:id`, crudInstance.findOne(), (req, res) => res.jsonp(req.result));
+  router.put(`/${routePath}/:id`, crudInstance.update(), (req, res) => res.jsonp(req.result));
+}
+
+setupRoutes(feed, 'feed');
+setupRoutes(feedStart, 'feed-start');
+setupRoutes(feedStop, 'feed-stop');
+setupRoutes(sleep, 'sleep');
+
+
 
 export default router;

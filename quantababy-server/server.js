6baa63c1e4dd0f794b4bfe3d2f3d75e93a181376
Sam Cowley <sfcowley@gmail.com>
2023-04-13 12:11:03 +0100

    Restructured thr project folder to seperate out the server and the client

---
+++ quantababy-server/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+// Import routes
+import router from './routes/routes.js';
+
+// ESM specific features
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const PORT = 8080;
+
+// Use JSON for request body
+app.use(express.json());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Use routes
+app.use('/', router);
+
+const startApp = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`⚡ Its alive on http://localhost:${PORT}`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+startApp();
